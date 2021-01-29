7cb8fdce133116fe4d74776c9708775ebd3ead23
GaurikShah <gaurik_shah@yahoo.co.uk>
2021-01-29 10:04:05 +0530

    31 dec 2020

---
+++ Dispersion-Trading-using-Options-master/proalpha_strangle_arb_backtest.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import py_vollib.black as black_model
+
+def import_data():
+    pass
+
+def create_instrument_list():
+    pass
+
+def calculate_implied_vols(option_price,futures_price,strike_price,dt,expiry_dt,flag,risk_free_interest_rate=0.06):
+
+    discounted_option_price=
+
+
+    pass
+
+def calculate_time_to_expiry()
+    pass
+
+
+
+def calculate_basket_implied_vols():
+    pass
+
+def calculate_dirty_correlation():
+    pass
+
+
+
+
+
+
+
+if __name__=="__main__":
+
+    pass
